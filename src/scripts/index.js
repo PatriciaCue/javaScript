801111a6575302d2a581bf5ec5136153bb413bb2
Patricia Cuevas <patricia.cuevas@the-cocktail.com>
2022-09-20 17:27:29 +0200

    REST Parameter, Desestructuracion de arrays

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,47 @@
 /**
- * DECLARACION DE VARIABLES
- * Las CONSTANTES tienen que estar inicializadas
- * y no pueden cambiar el valor durante el programa
- */
-const carId = 100;
-
-/**
- * LET VS VAR
- * Diferencias:
- * Cuando usamos LET en un buble no se puede llamar fuera de ese bucle
- * No se puede llamar antes a la variable si no se ha creado da ERROR
+ * REST PARAMETERS y DESTRUCTURING ARRAYS
  * 
- * VAR se puede usar fuera del bucle se declara de manera global
-*/
+ */
 
-//console.log(carID); // me va a dar un ERROR la variable se tiene que declarar antes
-let carID =42;
+ function myFun(a, b, ...manyMoreArgs) {
+    console.log("a", a);
+    console.log("b", b);
+    console.log("manyMoreArgs", manyMoreArgs);
+  }
+  
+  myFun("one", "two", "three", "four", "five", "six");
+  
+  // Console Output:
+  // a, one
+  // b, two
+  // manyMoreArgs, ["three", "four", "five", "six"]
 
-console.log(carNum); // me devuelve UNDEFINED
-let carNum =42;
+  /**
+   * DESTRUCTURING ARRAYS
+   */
+
+  //Ejemplo 1
+  let carIds = [1,2,5];
+  let [car1,car2,car3]= carIds;
+  console.log(car1,car2,car3);
+  // Console Output:
+  // 1  2  5
+
+  //Ejemplo 2
+  let carNums = [1,2,5];
+  let carA, remainingCars;
+  [carA, ...remainingCars]=carNums;
+  console.log(carA,remainingCars);
+  // Console Output:
+  // 1  [2,5]
+
+  //Ejemplo 3
+  let carIdenti = [1,2,5];
+  let restoCars;
+  [, ...restoCars]=carIdenti;
+  console.log(restoCars);
+  // Console Output:
+  //[2,5]
 
 
-console.log('Hello World!');
+
