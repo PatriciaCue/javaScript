d91bd067e09aa98aa4cbca11fa497e52a483a118
Patricia Cuevas <patricia.cuevas@the-cocktail.com>
2022-09-20 16:50:56 +0200

    Tipos de variables

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,24 @@
-import '../styles/index.scss';
+/**
+ * DECLARACION DE VARIABLES
+ * Las CONSTANTES tienen que estar inicializadas
+ * y no pueden cambiar el valor durante el programa
+ */
+const carId = 100;
 
-console.log('webpack starterkit');
+/**
+ * LET VS VAR
+ * Diferencias:
+ * Cuando usamos LET en un buble no se puede llamar fuera de ese bucle
+ * No se puede llamar antes a la variable si no se ha creado da ERROR
+ * 
+ * VAR se puede usar fuera del bucle se declara de manera global
+*/
+
+//console.log(carID); // me va a dar un ERROR la variable se tiene que declarar antes
+let carID =42;
+
+console.log(carNum); // me devuelve UNDEFINED
+let carNum =42;
+
+
+console.log('Hello World!');
