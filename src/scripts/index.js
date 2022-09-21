806975c73991a599d0ad549d12d0895f5b6591ae
Patricia Cuevas <patricia.cuevas@the-cocktail.com>
2022-09-21 16:44:38 +0200

    BOM y DOM

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,63 +1,24 @@
 /**
- * CLASES Y MODULOS
+ * PROGRAMMING THE BOM AND DOM
  */
 
-     //IMPORTING A MODULE
-     //Hemos creado la clase car, y la tenemos que importar en 
-     //el fichero que vamos a utilizar
-     import{Car} from './models/car.js';
+     //The Window Object
+
+     //Timers
+
+     //The location Object
+
+     //The document Object
+
+     //Selecting DOM Elements (.getElementById, .getElementsByClassName, .getElementsByTagName)
+
+     //Modifying DOM Elements (.textContent, .setAttribute, .classList, .style)
+     let element = document.getElementById('elementId');
+     element.textContent='new txt';
+     element.setAttribute('name', 'nameValue');
+     element.classList.add('myClassName');
+     element.style.color='blue';
 
 
-     let car8 = new Car(123);
-     console.log(car8.identify()); 
-     //Console Output: CarId: 123
 
-
-     //CLASS BASICS, CONSTRUCTOR AND PROPERTIES
-
-     class Car{
-          constructor(id){
-               this.id=id;
-          }
-          identify(){
-               return `Car Id:${this.id}`;
-          }
-     }
-
-     let car = new Car(123);
-     console.log(car.identify()); 
-     //Console Output: CarId: 123
-
-     class Car{
-          constructor(id){
-               this.id=id;
-          }
-          identify(suffix){
-               return `Car Id:${this.id} ${suffix}`;
-          }
-     }
-     let car2 = new Car(123);
-     console.log(car.identify('!!!!')); 
-     //Console Output: CarId: 123 !!!!
-
-     //INHERITANCE
-     class Vehicle{
-          constructor(){
-               this.type='car';
-          }
-          start(){
-               return `Starting: ${this.type}`;
-          }
-     }
-
-     class Car extends Vehicle {
-          start(){
-               return 'in Car start' + super.start();
-          }
-     }
-     let car6 = new Car();
-     console.log(car6.start); 
-     //Console Output: 'in Car start Starting: car ' 
-
-
-     //CREATING A MODULE
+     
