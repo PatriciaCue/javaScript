69aad8d8fe42a227ee0e8957483fb611757217cf
Patricia Cuevas <patricia.cuevas@the-cocktail.com>
2022-09-21 18:08:54 +0200

    Formularios

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,40 +1,56 @@
 /**
- * USING HTTP
+ * FORMS
  */
 
-     //HTTP REQUEST USING XHR
-     let xhttp = new XMLHttpRequest();
-     xhttp.onreadystatechange = function () {
-          if(this.readyState ==4 && this.status ==200){
-               console.log(this.responseText);
-          }
-     };
+     let form = document.getElementById('user-form');
 
-     xhttp.open("GET", "http://myid.mockapi.io/api/v1/users", true);
-     xhttp.send();
-
-     //HTTP REQUEST USING JQUERY
-     /** instalamos jquery npm instal jquery*/
-     import $ from 'jquery';
-                         //-----------end point--------------//
-     let promise= $.get("http://myid.mockapi.io/api/v1/users",
-          data=> console.log('data: ',data)
-     );
-
-     promise.then(
-          data=> console.log('success: ', data),
-          error=> console.log('error: ', error)
-     );
-
-     //HTTP POST USING JQUERY
-     let user ={
-          name:'Marta Gomez',
-          avatar:'marta.jpg'
-     };
-     let promise2= $.post("http://myid.mockapi.io/api/v1/users", user);
-     promise2.then(
-          data=>console.log('data: ', data),
-          error=>console.log('error: ', error)
-     );
-
+          form.addEventListener('submit', event=>{
+               event.preventDefault();
+     });
      
+     //Accessing form fields
+     let form2 = document.getElementById('user-form');
+
+          form2.addEventListener('submit', event=>{
+               let user = form.element['user'];
+               let avatarFile= form.element['avatar-file'];
+               
+               console.log(user.value, avatarFile.value);
+               event.preventDefault();
+     });
+
+     //Showing validation Errors
+     //<input name="user" placeholder="User Name"/>
+     //<span id="user-error"></span>
+     
+     let user= form.elements['user'];
+     let useError = document.getElementById('user-error');
+
+     useError.textContent='Invalid entry';
+     useError.style.color='red';
+     user.style.borderColor='red';
+     user.focus();
+
+     //Posting from JavaScript
+     
+     import $ from 'jquery';
+     let form3 = document.getElementById('user-form');
+
+          form3.addEventListener('submit', event=>{
+               let user = form.element['user'];
+               let avatarFile= form.element['avatar-file'];
+               
+               let posting={
+                    user:user.value,
+                    avatarFile: avatarFile.value
+               };
+
+               let promise= $.post('https://miurl.com', posting);
+
+               promise.then(
+                    data=> console.log('success: ', data),
+                    error=> console.log('error',error)
+               );
+
+               event.preventDefault();
+     });
